1f39734ef3e90f13b14c5c2e7f2a891fe605b756
Lalitha0108 <166808483+Lalitha0108@users.noreply.github.com>
2024-06-04 10:45:31 +0530

    Create array_pointers_char.c

---
+++ array_pointers_char.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char *arr[3]={"lalli","logesh","kiran"};
+
+    for(int i=0;i<3;i++){
+        printf("%s \n",arr[i]);
+    }
+
+    return 0;
+}
+O/P:-
+lalli
+logesh
+kiran
